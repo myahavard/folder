76b68607a059fab0fbc450833116150e7595f9b0
myahavard <mhavard@scmail.spelman.edu>
2014-11-29 22:35:18 -0500

    none

---
+++ project.cpp
@@ -0,0 +1,17 @@
+//
+//  project.cpp
+//  
+//
+//  Created by Mya Havard on 11/29/14.
+//
+//
+
+#include "project.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World" << endl;
+    return 0;
+}
---
+++ project.h
@@ -0,0 +1,14 @@
+//
+//  project.h
+//  
+//
+//  Created by Mya Havard on 11/29/14.
+//
+//
+
+#ifndef ____project__
+#define ____project__
+
+#include <stdio.h>
+
+#endif /* defined(____project__) */
